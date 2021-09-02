2d83f9bb8b5ec143ac68c6eb35454a96a99a92e5
bjames.12299 <bjames.12299@gmail.com>
2021-09-02 14:18:12 -0700

    Add project files.

---
+++ CosmicRayDetector/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace CosmicRayDetector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(51, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(195, 102);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start Watching Based On Screen Size";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(51, 214);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(195, 102);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Start Watching Based On Array Size";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.Location = new System.Drawing.Point(51, 182);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            2146435000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(195, 26);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            10000000,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 345);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ CosmicRayDetector/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace CosmicRayDetector
+{
+    public partial class Form1 : Form
+    {
+        //Stored values that are checked for Single Event Upsets (SEU)
+        byte[] seuBytes = new byte[(800 * 800) * 3]; // total is 15,360,000 bits
+
+        byte defByte = 111;
+
+        Thread DrawThread = null;
+
+        Graphics mDraw = null;
+
+        Graphics g = null;
+
+        Bitmap btm = new Bitmap(800, 800);
+
+        bool drawResults = true;
+
+        List<string> log = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+     
+        }
+
+        public void Draw()
+        {
+            if (drawResults)
+            {
+                Bitmap btm = new Bitmap(Width, Height);
+
+                mDraw = CreateGraphics();
+                g = Graphics.FromImage(btm);
+
+                g.Clear(Color.Black);
+
+                mDraw.DrawImage(btm, Point.Empty);
+            }
+
+            while (true)
+            {
+                for(int i = 0; i < seuBytes.Length; i++)
+                {
+                    if(seuBytes[i] != defByte)
+                    {
+                        // potentially add a save feature
+                        string status = "Detected Bit Flip (SEU) Value: " + seuBytes[i] + " " + DateTime.Now.ToString();
+                        Console.WriteLine(status);
+                        log.Add(status); // keep list of detections in a log for review later
+
+                        if (drawResults)
+                        {
+                            int x = i % (btm.Width * 3); //calculate x change
+                            int y = i / (btm.Width * 3); //calculate y change
+
+                            int rgb = x % 3;
+
+                            x /= 3;
+
+                            byte r = 0;
+                            byte g = 0;
+                            byte b = 0;
+
+                            if (rgb == 0)
+                            {
+                                r = seuBytes[i];
+                                g = seuBytes[i + 1];
+                                b = seuBytes[i + 2];
+                            }
+                            if (rgb == 1)
+                            {
+                                r = seuBytes[i - 1];
+                                g = seuBytes[i];
+                                b = seuBytes[i + 1];
+                            }
+                            if (rgb == 2)
+                            {
+                                r = seuBytes[i - 2];
+                                g = seuBytes[i - 1];
+                                b = seuBytes[i];
+                            }
+
+                            if (x >= 0 && x < btm.Width && y >= 0 && y < btm.Height)
+                            {
+                                btm.SetPixel(x, y, Color.FromArgb(r, g, b));
+                            }
+                            else
+                            {
+                                Console.WriteLine("Calculation Error"); 
+                            }
+                        }
+                    }                    
+                }
+
+                if (drawResults)
+                {
+                    mDraw.DrawImage(btm, Point.Empty);
+                }
+
+                Thread.Sleep(2500); //not necesary to be constantly checking
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StartIt(true);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StartIt(false);
+        }
+
+        public void StartIt(bool drawRes)
+        {
+            drawResults = drawRes;
+
+            button1.Visible = false;
+            button2.Visible = false;
+
+            numericUpDown1.Visible = false;
+
+            if (drawRes)
+            {
+                seuBytes = new byte[(Width * Height) * 3];
+            }
+            else
+            {
+                seuBytes = new byte[(int)numericUpDown1.Value];
+            }
+
+            for (int x = 0; x < seuBytes.Length; x++)
+            {
+                seuBytes[x] = defByte; // make sure the array is zero'd out
+            }
+
+            DrawThread = new Thread(Draw);
+            DrawThread.IsBackground = true;
+            DrawThread.Start();
+        }
+    }
+}
